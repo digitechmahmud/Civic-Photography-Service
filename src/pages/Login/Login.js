bb1a9538352662c52066f933af27b4f12f3c6e22
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2022-11-10 23:47:38 +0600

    final update

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -28,25 +28,8 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 const user = result.user;
-                const currentUser = {
-                    email: user.email
-                }
-                fetch('https://civic-photography-server.vercel.app/jwt', {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(currentUser)
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data);
-                        localStorage.setItem("civic-photography", data.token)
-                        navigate(from, { replace: true });
-                        form.reset();
-                    })
-                    .catch(error => console.log(error));
-               
+                navigate(from, { replace: true });
+                form.reset();
             })
             .catch(err => console.error(err));
     }
@@ -55,67 +38,53 @@ const Login = () => {
         signInGoogle(googleProvider)
             .then(result => {
                 const user = result.user;
-                const currentUser = {
-                    email: user.email
-                }
-                fetch('https://civic-photography-server.vercel.app/jwt', {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify(currentUser)
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data);
-                        localStorage.setItem("civic-photography", data.token)
-                        navigate(from, { replace: true });
-                        
-                    })
-                    .catch(error => console.log(error));
+                console.log(user);
+                navigate(from, { replace: true });
 
             })
-            .catch(err => console.error(err));
-    }
+            .catch(error => console.log(error));
 
-    return (
-        <div className="hero">
-            <div className="hero-content flex-col lg:flex-row">
-                
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <form onSubmit={handleLoginForm}>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" required/>
+    }
+            
+
+return (
+    <div className="hero">
+        <div className="hero-content flex-col lg:flex-row">
+
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <form onSubmit={handleLoginForm}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                                <small>Not a member yet? <Link className='text-blue-500' to="/register">Register</Link></small>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <div>
+                            <h2>Or login using:</h2>
+                            <div className='flex justify-center'>
+                                <Link onClick={handleGoogleSignIn} className='text-5xl mr-5'><FcGoogle /></Link>
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" required/>
-                                <label className="label">
-                                    <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
-                                    <small>Not a member yet? <Link className='text-blue-500' to="/register">Register</Link></small>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
-                            <div>
-                                <h2>Or login using:</h2>
-                                <div className='flex justify-center'>
-                                    <Link onClick={handleGoogleSignIn} className='text-5xl mr-5'><FcGoogle /></Link>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default Login;
